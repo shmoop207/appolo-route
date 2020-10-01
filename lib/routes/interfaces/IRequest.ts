19116ba9ccd9c274f2a78e66aef0e3244aa0227f
roman <shmoop207@gmail.com>
2020-10-01 20:13:58 +0300

    first

--- lib/routes/interfaces/IRequest.ts
+++ lib/routes/interfaces/IRequest.ts
@@ -1,9 +1,9 @@
-import {IRequest as IAgentRequest, IOptions} from "@appolo/agent";
+import {IRequest as IAgentRequest,IApp as IAgentApp, IOptions} from "@appolo/agent";
 import {IApp, IOptions as IEngineOptions} from "@appolo/engine";
 import {IRouteOptions} from "./IRouteOptions";
 
 
 export interface IRequest extends IAgentRequest {
-    app: IApp & { options: IOptions & IEngineOptions },
+    app: IApp & IAgentApp & { options: IOptions & IEngineOptions },
     route: IRouteOptions
 }
